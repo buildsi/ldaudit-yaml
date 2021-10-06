2cca9954da4d8ec46708f1ba472aa33f6effb8f4
vsoch <vsoch@users.noreply.github.com>
2021-10-06 16:36:50 -0600

    add colors and use box to make diagram smaller
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- generate_dot.py
+++ generate_dot.py
@@ -2,6 +2,7 @@
 
 # A small example of generating a dot file to visulalize the link graph
 
+import random
 import re
 import os
 import sys
@@ -14,6 +15,7 @@ node [fontname="Arial"];
 %s
 }"""
 
+
 def main(yamlfile):
 
     with open(yamlfile, "r") as stream:
@@ -22,50 +24,85 @@ def main(yamlfile):
         except yaml.YAMLError as exc:
             sys.exit(exc)
 
-    events = content.get('auditlib', {}).get("audits", {})
+    events = content.get("auditlib", {}).get("audits", {})
     if not events:
         sys.exit("Improperly formatted file, cannot find auditlib -> audits!")
 
+    # We will assign colors based on the parent (or white for root)
+    colors = [
+        "#" + "".join([random.choice("0123456789ABCDEF") for j in range(6)])
+        for i in range(len(events))
+    ]
+
     nodes = {}
     for event in events:
-        if event['event'] == "object_loaded":
-            nodes[event['identifier']] = event   
-            basename = os.path.basename(event['name']) or "root"
-            nodes[event['identifier']]['slug'] = re.sub("(-|_|[.])", '_', basename)
+        if event["event"] == "object_loaded":
+            nodes[event["identifier"]] = event
+            basename = os.path.basename(event["name"]) or "root"
+            nodes[event["identifier"]]["slug"] = re.sub("(-|_|[.])", "_", basename)
+            nodes[event["identifier"]]["color"] = colors.pop()
 
-    # Define the nodes
-    output = ""
-    for nid, node in nodes.items():
-        output += "%s [shape=circle]\n" % (node['slug'])
+    # Keep track of relations we've seen
+    seen = set()
+
+    # Keep track of child / parents
+    parents = {}
+
+    # Keep output string
+    relations = ""
 
     # For each event, see if the searched for library is then found
     for e, event in enumerate(events):
-        if event['event'] == "searching_for":
+        if event["event"] == "searching_for":
 
             # The next event is usually the find?
             next_event = None
             i = e + 1
-            while True:            
+            while True:
                 next_event = events[i]
-                if next_event['event'] != "object_loaded":
-                    i+=1
-                    if i > len(events) -1:
-                       next_event = None
-                       break
+                if next_event["event"] != "object_loaded":
+                    i += 1
+                    if i > len(events) - 1:
+                        next_event = None
+                        break
                 else:
-                    break       
+                    break
 
             # This is a child library of the event in question
-            node = nodes.get(event['initiated_by'])
+            node = nodes.get(event["initiated_by"])
             if next_event and node:
-                name1 = nodes[event['initiated_by']]['slug']
-                name2 = nodes[next_event['identifier']]['slug']
-                output += "%s -> %s\n" % (name1, name2)
-                
-    print(template % output)          
+
+                # Keep a lookup for the parent node for coloring
+                id1 = nodes[event["initiated_by"]]["identifier"]
+                id2 = nodes[next_event["identifier"]]["identifier"]
+                parents[id2] = id1
+
+                name1 = nodes[event["initiated_by"]]["slug"]
+                name2 = nodes[next_event["identifier"]]["slug"]
+                if "%s->%s" % (name1, name2) in seen:
+                    continue
+                relations += "%s -> %s\n" % (name1, name2)
+                seen.add("%s->%s" % (name1, name2))
+
+    # Define the nodes
+    output = ""
+    for nid, node in nodes.items():
+        shape = "box"
+        if node["slug"] == "root":
+            shape = "circle"
+
+        # Figure out the parent for the color
+        parent = parents.get(node["identifier"])
+        color = ""
+        if parent:
+            color = ',style=filled,color="%s"' % nodes[parent]["color"]
+        output += "%s [shape=%s%s]\n" % (node["slug"], shape, color)
+
+    output = output + "\n" + relations
+    print(template % output)
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         sys.exit("Please provide a yaml output file to parse!")
     main(sys.argv[1])
-
